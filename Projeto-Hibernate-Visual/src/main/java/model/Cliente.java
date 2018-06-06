7f24f9c879aea269f0be4a961abd1aaf7165f27b
jhonyjota <jhonysousak@hotmail.com>
2018-06-06 20:38:56 -0300

    Exercicio

---
+++ Projeto-Hibernate-Visual/src/main/java/model/Cliente.java
@@ -0,0 +1,122 @@
+package model;
+
+
+import javax.persistence.*;
+
+@Entity
+public class Cliente {
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id_cliente;
+	@Column(name="nome")
+	private String nome;
+	@Column(name="endereco")
+	private String endereco;
+	@Column(name="bairro")
+	private String bairro;
+	@Column(name="cep")
+	private String cep;
+	@Column(name="cidade")
+	private String cidade;
+	@Column(name="estado")
+	private String estado;
+	@Column(name="telefone")
+	private String telefone;
+	@Column(name="celular")
+	private String celular;
+	@Column(name="rg")
+	private String rg;
+	@Column(name="cpf")
+	private String cpf;
+	@Column(name="sexo")
+	private char sexo;
+
+
+	public String getNome(){
+		return nome;
+	}
+	
+	public void setNome(String nome){
+		this.nome = nome;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getCelular() {
+		return celular;
+	}
+
+	public void setCelular(String celular) {
+		this.celular = celular;
+	}
+
+	public String getRg() {
+		return rg;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public char getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(char sexo) {
+		this.sexo = sexo;
+	}
+	
+}
